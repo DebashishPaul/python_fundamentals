47758c82cf5fa5e560678adfe014c7c13ca3b3f8
Debashish Paul <debashish333paul@gmail.com>
2019-05-16 23:22:34 +0600

    Add files via upload

--- practice.py
+++ practice.py
@@ -1,1 +1,14 @@
-print('hello world')
+print("the lab's")
+
+x=30
+if(x==30):
+    print('done!!!')
+
+#loop
+for loop in 'python':
+    print('the current letter:',loop)
+
+#loop2
+x=['bob','rana','champ','charly']
+for loop in x:
+    print('current name: ',x)
