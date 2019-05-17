48908a3b480b3d0aaa4a00fb6c1b575ab707d325
Debashish Paul <debashish333paul@gmail.com>
2019-05-17 22:03:46 +0600

    Add files via upload

--- math_module.py
+++ math_module.py
@@ -5,3 +5,10 @@ print (math.ceil(123.12424))
 #concatinate
 x='hello world'
 print (x[:6]+'bangladesh')
+
+#string
+var1 = 'Hello World!'
+var2 = "Python Programming"
+
+print(var1[10])
+print("var2[1:5]: ", var2[1:5])
